ec005e99a1e84ea58ddd78bc932f81e9ce678dc7
Michael Sverdlin <Sveder@users.noreply.github.com>
2020-12-19 23:58:23 +0200

    Add files via upload
    Awful code for day 19

---
+++ 2020/19/19.py
@@ -0,0 +1,639 @@
+input = """90: 86 86
+122: 86 1 | 99 20
+116: 86 58 | 99 75
+20: 86 123
+62: 99 95 | 86 113
+81: 76 99 | 90 86
+106: 120 86 | 93 99
+73: 99 72 | 86 45
+117: 131 99 | 72 86
+92: 86 96 | 99 98
+13: 3 99 | 118 86
+56: 90 86 | 58 99
+85: 72 99 | 51 86
+51: 99 99 | 86 86
+59: 99 25 | 86 62
+65: 99 15 | 86 97
+112: 86 13 | 99 38
+46: 33 86 | 2 99
+10: 67 86 | 68 99
+33: 120 99 | 76 86
+38: 35 86 | 125 99
+26: 86 10 | 99 55
+1: 33 99 | 60 86
+8: 42
+16: 51 86 | 93 99
+107: 40 99 | 2 86
+40: 17 120
+34: 86 82 | 99 127
+88: 93 17
+2: 99 51 | 86 120
+32: 100 99 | 7 86
+113: 86 127 | 99 82
+14: 73 86 | 44 99
+25: 86 101 | 99 56
+130: 110 86 | 109 99
+19: 86 4 | 99 49
+30: 86 92 | 99 70
+27: 17 86 | 86 99
+94: 47 86 | 53 99
+115: 86 107 | 99 84
+15: 76 99 | 58 86
+58: 86 99
+105: 130 86 | 32 99
+71: 120 99 | 131 86
+12: 99 131 | 86 82
+60: 72 86 | 93 99
+84: 86 102 | 99 80
+44: 99 76 | 86 72
+125: 76 99 | 131 86
+18: 99 71 | 86 52
+129: 37 86 | 111 99
+102: 99 131 | 86 76
+66: 86 105 | 99 41
+99: "a"
+9: 99 18 | 86 65
+131: 17 99 | 99 86
+39: 76 99 | 93 86
+64: 115 99 | 114 86
+57: 86 48 | 99 94
+35: 72 86 | 51 99
+0: 8 11
+77: 86 83 | 99 106
+118: 72 86 | 75 99
+47: 99 103 | 86 85
+23: 99 27 | 86 76
+48: 119 99 | 78 86
+49: 86 51 | 99 45
+67: 86 120
+61: 86 72 | 99 127
+108: 72 99 | 72 86
+95: 86 58 | 99 90
+83: 86 27 | 99 131
+75: 86 99 | 99 99
+101: 51 99 | 27 86
+103: 90 99 | 90 86
+128: 86 69 | 99 33
+70: 99 14 | 86 19
+52: 127 86 | 90 99
+21: 86 24 | 99 59
+22: 86 63 | 99 12
+42: 79 86 | 66 99
+97: 51 17
+104: 86 33 | 99 28
+100: 99 16 | 86 39
+72: 99 86
+78: 86 43 | 99 50
+55: 86 6 | 99 34
+45: 99 99
+5: 86 46 | 99 77
+93: 99 99 | 99 86
+6: 131 99 | 51 86
+110: 71 86 | 28 99
+68: 90 86 | 27 99
+29: 87 86 | 122 99
+80: 86 93 | 99 131
+54: 120 86 | 75 99
+43: 93 86
+98: 99 103 | 86 117
+7: 101 86 | 88 99
+127: 17 86 | 99 99
+96: 86 12 | 99 61
+41: 99 5 | 86 112
+79: 86 57 | 99 21
+11: 42 31
+86: "b"
+111: 45 99 | 72 86
+63: 76 99 | 127 86
+124: 86 81 | 99 116
+28: 86 75 | 99 58
+82: 99 86 | 86 99
+121: 64 86 | 74 99
+87: 86 124 | 99 104
+74: 99 26 | 86 9
+31: 126 99 | 121 86
+50: 99 72
+119: 34 86 | 36 99
+36: 86 82 | 99 75
+91: 86 131
+3: 58 99 | 27 86
+114: 99 129 | 86 22
+24: 86 89 | 99 128
+53: 91 99 | 95 86
+126: 29 86 | 30 99
+109: 99 108 | 86 23
+17: 86 | 99
+76: 86 86 | 99 86
+120: 17 17
+89: 54 86 | 37 99
+4: 99 131 | 86 58
+69: 27 17
+37: 99 58
+123: 86 76 | 99 82
+
+bbababbaabbaaabaaaabbabbbbbababbbababaaaabbaabaaaaaabaaaabbaabba
+aaabbbabbabbbbbbaabbabababaaaaabaaabaaaaabaaaabbbbabbabb
+babaabaabbabaaaaabbababb
+babbabbaababbaaaababbaabbbbbaabaabbbababaabbbabbbabababaabbabbabaabbbaababbbbbbb
+bababbbabaaabbaabbababab
+aaaaabaaaabbaabaaaaabbaa
+aabbbaaaaababbaaabaabbbbabbbaaaaabbaaaab
+baaaabaaaabbababbaaabbab
+ababababaaaaabaaaaabbaaa
+baabaababaaabbaabbbababb
+abbbabbbbbabaaabaabbabbb
+aaababbbabababbbaabbaaaabababbaa
+babbaaabbaaaabaababbbabb
+bbbabbbabbabaaaabaabaaab
+bbbbaaabaaaabbbaabbbbabb
+aaaaaaaaaaaaabaabaaaabbb
+babaabbababbabaababbaaba
+bbabaaaabaaabbaaaabbbbbbbabbabbbbaaaabab
+babaabbbbbbbbbaabbaaabaa
+baabbbabababbababaabbbaa
+baaaabaaaabaaaaaaaaaaaab
+aaabbbbbbbabbbabbbbbabbb
+bbabaaabaaababaabbbbbbba
+baaabaaabbabbaaaaaaababb
+abbabaabbaaaaaaaaabaabba
+aaaabababbbbbbaabababbab
+bbbbbbabababbaabaabbbabaaaabbabbaabbabbbbaababbbabaabbbbabbabaabababaabaaaaaabab
+bbbabbaaaababbabaabaabbbabbbabab
+bbbabbbababbbbaabbbbbbab
+baabaaaaabaabbbbaaabaaaabbbbbbab
+bbbbaaaaaabaaaaaabbbabaababbaabb
+bbbbbabaabaaaaababbaaabbbaababbbbbaaabaa
+aaabaabaaaabaaaaababbaaaaaababbbaaababab
+bbaaaababbbaaabbabbabbabbabbbbabbababaabbbabbabbabaaabbabaaaabba
+aababbbaaaabbbaabbbbababbabbaaaa
+abbbaabbabaaaaababaaaaaa
+babbabbabbaabababbabaaaaaabbbabbbabaabbbbabbbaabbaaaaaba
+aabbbaabaaabbabbbbaabaaabbbabbababbaababababbbba
+baabbabbbaabbbbbbbaaaaaababababaaabbbbabaabaabbaaaaabbab
+bbabaaabbbbbaaaababbabbb
+baabaabaaababbbabaaababa
+aaaabbbbabbbaabbababaaaa
+babbababaaaababababaabab
+bbbaabbbabbbabaabbbabbbaaabbaabbabbbbbbbaaaabaaabaaabbaabbbbabbb
+aabbaabbabbabbbabaaaabba
+abaabbaaaaabbabbaaabbbabbabbaaaa
+bababaabbaaabaaababbababbbbbbabbbaaabaab
+aaababbbaabbaabbbbaaabbaaababaaabaabbbaa
+abbaaababbaaababbaaaabba
+bbaababaaabbabaaababbaabbaabbbbaaabbaabbabaaabbabbabbbbbabbbaaabbbababbb
+bbaaabbbbbaaabbabaaaabba
+abbbabaabbaabaaaaaabbaaa
+bbbabaabbbbbbbaababbbbaaabbbabba
+abbabbaaaaaabbbbbbabaaabbaaaaaab
+bbaababaaaababaabaabbababbbbbaab
+aabbababaaabbbaaaaabbabbaabbbbbbbabbbabb
+aaabbbaaaababbabbababbbb
+aababbababaaabaabababbbb
+bbabbbabbbabbbabababbbba
+aaabaaaaabbbbbbbbaaaaaab
+babaabbbaaabbbabbaaaaaab
+bbaabbbaaabbababbbaaabaa
+aabbbaaaaaaaaaaaaaaaabaaaabbbbbabbabaaaaabbbbbab
+abaaaaabaabbbaaaabbbaaab
+abaabbabbabbbbbabbababaa
+baaabbaaaabbbabbaaaabaaa
+abbbbbaabbaaababbbbabaab
+aababbaaaabbbbbabaababaabababaabbabbbaba
+abaabaaabbabababababababbaabaaabbbaabaabaabbbababbbabbbabbabbbbbabababaabbabbaba
+aaaaaabaaaaabababbbabbabbbaabbabaababbbb
+babaaaababaaababaabbabba
+ababbaaaabaaabbbbababaaa
+baababbbabbbbaaabaababbbabbbbaababaaabaaaabbaabababababaaaaababa
+bbaabbababbabaabbbabbabb
+bbabbbbaababbaabbbbabbbbbabbabaaaaabbbababbbbaabaaaaaabbbababbbbbabbbabb
+baaaaaaababaaaabbbbbaaaaabaabbbbbaababbb
+aabbabaaabbbbbaabbbaabab
+ababbabbbbaabaaabababaabaaaabbab
+aaabbabaaaaabbbbabbbbaaa
+bbaaaaabbaaaabaaabbbaaaa
+abbbaaaaabbbabbbabbabaaa
+bbaaaabbaaaababababaababaaaabbbaaabaabaaaababbabbbaaaabaabaaabbaababbbaaabaabbaababbbaabaabaabbb
+bbaabbaaaababbababbaaabbbabbbababbbbbbaabbaabababbababaababbaababbbaaabbaabbbbab
+aababbabaaabaaaabbaabbaa
+babaaabbaaabaababbabbbbabbbaabaabbaabbbababbabaabababaabbbababbabbbbaababbaabaaa
+baabbabbbbaabbbaaabbaaab
+abaaaaabbbaababaabbaabab
+aaabbbbbaaababaaabababaabbaababb
+bbaaababbaabbbbababbbbbabbaabbabbbbbaaabbbabbabb
+bbabbbbaabbbbbbbbaaaabba
+aababbabaaabaababaabbabbabaaaabaaabbbabaaabbbabbbabaaaaa
+babbbbbbaabbbaabbabbbabb
+baabbbbaababbaabaaabbabb
+aaabaabaabbbaabababbbbaaaaaabaabbabaabbbabaaabbbaaaabababaababbbababbabaaaaaabab
+aabbabaabaaaaaaababbababaabaabba
+ababbababbaabbbaabaabaababbaabbbaaaabbaa
+aabbababaabbbbbbbaababaababbabba
+baabbabbbaaaaaaaaaaabaaa
+baabbbabaaabbabbbbbbbabb
+babaabaaaabaaaaabbbaaaab
+baaaaaaababbbbbaabbbbbab
+bbabbbabababbaabbaaababa
+bbbbabababaaaaababbabbbababbabaaabbaabababbababaaaaabbab
+aabbbbbababbabaaabababaaaaabaababbbabbab
+babbbbaabaabbbbabbbbbabb
+aaabbbabbabaabbbabaaababbaaabaab
+abbaaabbbbaaabbbbabbbbbbbbbbabaa
+bbabbabaaaabbabbbbbababababbaaabbabaaaaababaaaba
+bbaabaaaaabaaababaababbb
+baabbaaaaaababbababbaabaabababbbabaaabbbbbbaaabbbbababaababaabbbbbabaaababbaaaabbbaaaaaa
+aaabaaabbbbbaaaabbbbabba
+abbbaabbbabbbaaaaabaabab
+aaabbbaabbaabbabbaabbbbbbbabbaaaaaaabbbaaaaaabab
+aaaaaaaaaabbaabaaabbbbaabbaaaaabaaabbaababbbbbab
+bbbbababbabababbbbababba
+abaaaabaabbbbaaaabbbbbbbbaababbabbbbaabbaabbbbbabaaabbbaaaaaabab
+bbbbbbaabaabaaaaaaaaabbaaaaaaababbabbabaabbabbab
+abbbbbbbbaaaabaaaababbabbbbbaabb
+bababaabaababbbaabbaabab
+abaaaabaabaabbabaaabbbbbbbaaaaaa
+aaaaaabaababababbbaabbbabbaaaaba
+abaabaabbbababbababbaabb
+baaabbaabbbaabbbbabbabbb
+baababaabaaabaaaabbabbbabababbaabbbbbbba
+bbbaabbabbbabaabbaaabbaaaabababaaabbbbaaababbaab
+abaabbbbabbabbbbaaaaabab
+bbbabaababaabbbbbbbaaaab
+abbaaabaaaaaaabaabaaaabaabbaaabbabbabbbbaabaabab
+aabaaaaababbbaaaaaaababb
+aaabbbaaabaabbbabbbbabaa
+aaaaabaababaabbabaaaabaaabbaaaaa
+aaaabaabababbbbbaaaaaaaaaabbbbbbbbbbbbababbbbabbbaaabaab
+bbabbbabbaabbbbaaaabaabaabaaabbbaaaaaaababbbaaab
+aabaaabaaabbababbabababa
+babbbaaaaabbbaabbbabbababbbbbbbaabbababa
+abbaabaabaabbaaaabaaaaaaababbaab
+abaaaababbaabababaabbbababbabaabaaababbbbbbbabba
+bbaaaabababbabababbabaabbbaaabbbbbbabbaa
+aabbaabbaaabbbaabaaaaaab
+bbaabbabaaaabbbabbabbabb
+abbaaabbabbbaaaabbbbbaaabbabbabababbbbbbabaabbbbbabbaaaaabaababb
+ababbaabbabbbbbbbbabaaaabbbbababbaaabbbabaabaabb
+aaabbabaaabbbabaaaaabaabaaababbaabbbabbababbbbabaabbabbb
+abbabbaaabababababbbabaaaaaabaabbabaabab
+babaabaaababbaaaaabbaaab
+aaaaaababababaababbaaaab
+ababbabaaababbaaabbbaababaaaaabb
+bbaaabbabaaaabaabbabaaaaaaaababbbbbabaaa
+bababbbabbaabbabbbbbabba
+bbaaabbaaabbbbaabababaabbabbaaaa
+aaaabaabaaaaabbaababbaabababbaaa
+ababbabaabbabbbbbbaababb
+aabbbbbbbaaabbbababababa
+bababbbaabbabbaabaaaabab
+bbaaabbbaabbbbbaaabaabba
+aabbbbaaababbbabbbaabbbb
+baaaabaaabaaabbbababbbabaabbbabbaabbbaabbbbbbbabaaaabbaababbaabaaababbbb
+babbbbabbbbababbabaaabba
+babbbbaaababbbbbbabbbaaabaabbaab
+bbbbbbaaaabbbabbaaabaaaabbbabaaaaabaabab
+babbbbbabbabaaaaaaaaabbb
+ababbbabbabaabbaaaabbabbaaaabbaa
+bbbabbbabababbbabbaaabbbbbbbaaba
+abbabbaaabaaababbaaaaabb
+bbababbabababaaaaababbbbbabbbaabababbaabaabbbaababbaaaba
+babbababaabbbaaaabbaaaab
+abbaaabbbaaaaaaabbababab
+bbbabbbabbabbabababbbbab
+babbbaaabababaabaababbbababbbbbaabbbbaba
+bbbababaabaabaabababbbbbabaaaababbabbbabbbaaabaababbaaaaabaaaabbaaaabbab
+babababbabbabbbaaaaabbab
+baabbabbabaabbaaababbbbb
+aabbabaaaaaaabbaabbabbbaababbaabababbbbbbabbbababbbaaaba
+aababababbbabbbaabbaaaaa
+aabababaaabaaabaaabbaababbbabbaabbaaababbbbbbbababababbaabbbbbab
+abbbaabbbbabbbbaabbabbab
+aaaabbbbaabbaabbbabaaaaa
+aababbababaabbbaaabaabba
+baaaaabaaaaaabbababaabbbabaaaabbabaabbbaabaaabbaabaababaaaabbaaa
+ababbaaabaababaabbbbbababaaabbbb
+babbabababbbaaaabbbaabaa
+baabbabbbaabbabaaabbbbaabbbaabbaabbabaababaaabaabbaababbbaaaabbb
+abaabbbbaaaabaabbbbbbbba
+baabbabaaaababaaabbaabbbbbbbbbbb
+abbaaaabababbbbabababaabbbaaabbaababaabbbaaaaaaaaabaaaab
+bbbabbabaaaabbababbaabababaaaabaaabbaaaabbbababa
+bbbababababbbbbbababbbba
+bbbbbbaaaaabbbaabaababab
+aaaabbbaabbaaababaaababa
+bbaaabbabbabaabaaaabaaaaabaaabbabaaababb
+ababbabaabaabbaababaaaaa
+bbbaabbaaaababbaabbbbabaaabaaaab
+abbbabbbbbbabbaaabbaabbbababbbbaaabababb
+bbbbbaaabaaabaabaabbaaab
+bbaabbababaaababaaabbbabaaaababaabbaaababaabaaab
+abbbabbbaabbbbbabbababbb
+bbbaabbabbabbbabbaaaabba
+bbabaabaababbbaababababaaabaabababaaaaaabaaababb
+babaabaaaaaaabaaaabaabba
+babbbbbaaabbbaaaaabbababbaababba
+aababbaaababbbbbbabbabba
+bbabbbabaabbaabbbbbbabaa
+ababbababbbababaababbbaa
+aaabbbaabbabaabaababbbba
+bbaabbabbbabbaaabaabbbababaaabaabbbbbabbbbaaabaaababaaba
+baababaabbaaabbabaaababb
+abbaabbbababababaaababbbbaabaabb
+baabbbbbaaabaababaababba
+babaabaababaabbbbaabaabaaaababaababbabba
+bbaaaaabaababbaaaabbaabaaabbabbbbababbaa
+abbaaabbaaabbbbaaabbbbaaabbaaaabaabbaaabbaababbbabbabbabaaaababb
+aaabbaabababaaaabaabaaab
+bbaabbaabbbabaaabbabbbbbbbbabaabbabaaabababbbbabbaabbabbbbbabbaa
+bbbabbbbbbabbbaabaababab
+bbabbbbabbaabbbababbabba
+aaabbbaabaabaababaabbabababbaaaabbbbbbba
+babbaabababbabababbbbbbaababaabbbaabbbbababaaaabbbaaaaababbabaaabbbbaaaaabaaababbbbaabba
+babbbabbbaaaaaaabbaabbbbabbbbabbaaabababaabbbbab
+bbaaaaababaaabbaabbbaaaaaababbabbbaabaabaaabaaabbabaabab
+baabbbbbabbaaababbbbaaabbbbbbaabbbbbbaabbaaaaababbaaaaaaaabbabba
+bbabbbaaabaaabbbabbbabaaaababbaaaabbbaabababbbbabbababaaaaaababb
+baaabaaaaabbababbbabbbbaabaabbbaaabbbaabbaaabbabbaaabaab
+aaaaaabaaaaabbbabbbaabaa
+bbaaabbbbabaaaabbabaaaabaabaaaaababaababaaaabaaaaababbbb
+babbaaababbbaabaabbbbaab
+aaabbaababbbbbabaaabbaaaaabbbbbaabaabaababababababbabbbb
+babababbbaabaaaabaaabbbb
+babbabaaaaabbbabbbbbbbaabbbaaaaa
+aabbabaabaaabbbaaaaaabab
+bbbabbaaabbbbabbbababbabaaabababaababaab
+bbabaaaabaabbabbbbbabaabaaababbb
+baababbbbaaaabbabbaabbbbbbbbbaaabbbbaaba
+aabbaababaaaabaabaabbaba
+bbbabbaababaabaaabbbaaaaabaaaabaaabaaababababaaabbababbbbbbbabba
+bbabaabaabaaabaabbabbaab
+aabaaaaaaaababbbaaababbbbbbaabab
+baaaabaaabbbabbbabaaababbaabbaaa
+baabbbbabababbbaaababbbabbabbbbabbaabbbaabbabababaaabababaabaabbbabbabba
+baabaabaabababaabaaaabbb
+abbaabbbbabaabaaababbababaabbabbbabbbbabbbbbaabababbaabb
+abbbabbbbbbababaabbbabbabbbbbaab
+abaaabbbbbbaabbbaabbaaab
+ababbbabbbabbbaaabaaabaababababa
+bbbbbbaaabbbaaaaaaabbabaabbbbaabbbbaaaab
+bababbbbbabbbbbbbaabbbbbbbababbbbabbabbbabbbbaabbaaaabba
+baaabbaaaaaaaababaaabbbaabaabaaabaabababaaabbaaaabbaabaa
+aabbaabbbbbabaababaabaaaababbaaabbabbababbbbaabb
+abbbbbaabbbabbbbbbbbaaba
+aaabbabbbbbabbbababaaabb
+abaabaaaaabbaaaababbbaab
+aaaabaabbbbbaabababaaaaaaaaaaabb
+bbbababaabbbabaabaababba
+abbbaabbabaaababbbbbbabb
+babbabbbaaaaabbbaababbabbaabbbbbbaaabbaaaabbbbbaabaababbbbaaaaba
+aaaaaababbbababababaabab
+bbaabaabbbaaaabbabbababaaaaaababaabaaabb
+aabbabaababaaaababbbabab
+ababbbababaabbbbbaaaabbbbbbbbaababbbbaaabbabbbaaabbbabbbbaaaabbbaaaaaaaaababaaab
+bbbabaabaaaaabbaaaabaabb
+babbaaabbaabaababbbbaaba
+bbabaabababbbbbaabaabaaabbaaabbabbbabaaa
+abbabaabbaaaabaabbbaaaba
+aabbabaabaaabbbaaaabbbabbabababbbbbaabbb
+aabbbbbbabababababbbbbab
+abbabbbabbabaababaaabbbb
+baaabbaaababbaaabaabaabb
+bbbabbbaaaabaaaaabaaababaaaabbbbaabbaaaaabbbabab
+aabbbabaaaababaabaaaaaba
+abbbabbbababbabbbbaabbaa
+abbbbbabaabababbbabababa
+bbaabaabaababbaabababaaa
+bbabaaabaababbbaaababbbaaaababba
+abbaabaababbababaaaabbbaabbabaababbbaabaaaabaaabaabbbbab
+babbbaaabbabbbaababbbbaaaabaababaaaabaaa
+babababbabaaaabaaabbbabaaabbbabababaaaabbbabbaab
+aabbbaababbaaabaababbaababaaabbbaaaaaabaababbaaaaabaaabbbbababaa
+abbbabbbaaabbbbaaabaabab
+abaabbaaaabbaabbbbbaaaaa
+bbabaaabaaaabbbbaabbbaaaaabbbbbbabbbbbbabbbabaaa
+bbbabbaababbbbbabbaaaaabbabaabaaababaababaaaaabb
+abbbabbbababbaaabbabaaaaabbabbbabbabaabbbaaaaabbabbabaaabbbbbbbb
+bababbbaaaabaabaabbababb
+abbbbbbbabababaaabbaababbabaaababbaaaaaa
+abbaabbbabaaaaabaaaaabbaabaaaaabababbbababbbabab
+abbabbbbbbabaaaabbbabbbabbbbaaba
+abbaabbbbaabaababaababba
+bbabaaababaabbbaaaaaabbb
+baabbabaaaaabbabaabbbbabbabbabbabbaaaaaa
+babbbbbbababbababbabbbabaabbaaab
+aaabaaabbbbabbbbaabbabbbbaaaaaabaabbbaabbaababaa
+baabbbbbaaabaababababbab
+babbaaabaaaabaababbabbaaaabaabaa
+abaabbbbbbabaaaaaabaabab
+aaaaabbaabababaabbbbbababbbababbbbaababb
+abababababaabaaabbaaababbabbabbb
+abaaaaabbbbbbabaaaaabbaa
+aaaabbbbbbbbababbbabaabb
+bbabbabaabaaabaaaaababab
+aaabbbbbabbabbbbbabaababbbaabaababbababb
+bbabaabaabbabbaaaabaabba
+abbabaaababbaabaaaaabbbabbaaaaabbbaabbabbababaaabaabbaabbbbaaabaaaabbbaabbbbabba
+ababbbabaaabbabbaaabbabaaabaabba
+bbbabbbbbabbbbbbabaaabaaaaababbbbababaaa
+babbabaaabbaaabbabbabaaa
+babbbbaaabbbbbaaabaaabaabbbbababbaaababb
+abbabbbbabababbbbbabbaaabaaababb
+babbababbbaababaabbabbab
+bbaaababbbbbaaababbbbabb
+baaaabaababaabbbabaabbbaaabbbaabaaaabbab
+abbaabbbbbaaaaabbbaabbabbaabbaab
+aaaaaababbaababaaabaaabababbbabb
+baaabbaabbbababaaababbbaaaaaabba
+bbbbabaaaaabaaaaaaababbaaaababbbbbaabaaaaabaaaabaaaabbaabbbbbbbb
+abaaabbbbaaabaaababbabba
+aaaaabaaaaabbbaabbabaaaaabababaaabbbbbaabbaaaaba
+ababbabababbbaaaaaaaabbb
+aaababaaabaabbaababbabbb
+ababababaabbbabbbaaabaab
+baabbbabbaaabaaabaaabbbabbabbabb
+bbbabbaabbabbababaabbbbaabaabbabaabaaabbbaaabbbb
+aaabbbabaaabbbbbabbabbbbbbbaaaaa
+aaabbababbaaababaaaabbaa
+bbbabbaaababbbabaaabaabb
+baababbabbbabaaababaaababababbaaaababaabbbbaaaabbabaaababbbabaababbabbbbbaaabbab
+bbbabbaababaabbabbbbbaaa
+abababaabaaaabaaabababbbbaababbbbbabbaaaaaabbaabbbbabababbabaabb
+babbbababaaabbbabaababbbaaabababaaaababbbabbaabbabaabbba
+bbbabbaabaabbbbaaaaaabaabaaabbab
+bbbabbbbaabbaabaaaaabbbbbaabbabbabbaabaababbaabbaaabbaaa
+aaababbbbaaaabaaabbabbbbabbbbaba
+aaaaabbababaabbabaaaaaaaaaaaaababbbbbbaaababbaaaaabaabab
+babbababbaaabbabbabbaaaabbaabaabbaaabbab"""
+#
+# input = """42: 9 14 | 10 1
+# 9: 14 27 | 1 26
+# 10: 23 14 | 28 1
+# 1: "a"
+# 11: 42 31
+# 5: 1 14 | 15 1
+# 19: 14 1 | 14 14
+# 12: 24 14 | 19 1
+# 16: 15 1 | 14 14
+# 31: 14 17 | 1 13
+# 6: 14 14 | 1 14
+# 2: 1 24 | 14 4
+# 0: 8 11
+# 13: 14 3 | 1 12
+# 15: 1 | 14
+# 17: 14 2 | 1 7
+# 23: 25 1 | 22 14
+# 28: 16 1
+# 4: 1 1
+# 20: 14 14 | 1 15
+# 3: 5 14 | 16 1
+# 27: 1 6 | 14 18
+# 14: "b"
+# 21: 14 1 | 1 14
+# 25: 1 1 | 1 14
+# 22: 14 14
+# 8: 42
+# 26: 14 22 | 1 20
+# 18: 15 15
+# 7: 14 5 | 1 21
+# 24: 14 1
+#
+# abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+# bbabbbbaabaabba
+# babbbbaabbbbbabbbbbbaabaaabaaa
+# aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+# bbbbbbbaaaabbbbaaabbabaaa
+# bbbababbbbaaaaaaaabbababaaababaabab
+# ababaaaaaabaaab
+# ababaaaaabbbaba
+# baabbaaaabbaaaababbaababb
+# abbbbabbbbaaaababbbbbbaaaababb
+# aaaaabbaabaaaaababaa
+# aaaabbaaaabbaaa
+# aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+# babaaabbbaaabaababbaabababaaab
+# aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"""
+
+import functools
+import pprint
+
+rules, cases = input.split("\n\n")
+rule_dict = {}
+expanded_dict = {}
+
+
+def expand(rule_number):
+    global rule_dict, expanded_dict
+    stmt = rule_dict[rule_number]
+
+    if rule_number in expanded_dict.keys():
+        return expanded_dict[rule_number]
+
+    if stmt in ('"a"', '"b"'):
+        expanded_dict[rule_number] = stmt[1]
+        return [stmt[1]]
+
+    allowed = [""]
+    end_allowed = []
+    new_allowed = [""]
+    for sub_rule in stmt.split(" | "):
+        for x, rule_part in enumerate(sub_rule.split(" ")):
+            new_allowed_cache = list(new_allowed)
+
+            # if rule_number == 2:
+            #     import ipdb;ipdb.set_trace()
+
+            int_rule_part = int(rule_part)
+            expand(int_rule_part)
+
+            new_values = expanded_dict[int_rule_part]
+
+            for i in list(new_allowed):
+                for j in new_values:
+                    # print(i + j)
+                    new_allowed.append(i + j)
+
+            new_allowed = list(set(new_allowed) - set(new_allowed_cache))
+
+        end_allowed += list(set(new_allowed))
+        new_allowed = [""]
+
+    # print("Expanding rule number:", rule_number, ' stmt ', stmt, ' allowed ', end_allowed)
+    print("Expanding rule number:", rule_number, ' stmt ', stmt)
+    expanded_dict[rule_number] = end_allowed
+
+
+for rule in rules.split("\n"):
+    rule_num, content = rule.split(": ")
+    rule_dict[int(rule_num)] = content
+
+# Patch:
+rule_dict[8] = "42 | 42 8"
+rule_dict[11] = "42 31 | 42 11 31"
+
+print("Rule 0:", rule_dict[0])
+
+import ipdb;ipdb.set_trace()
+
+expand(42)
+expand(31)
+
+import ipdb;ipdb.set_trace()
+
+# Match to cases:
+matches = 0
+
+import re
+import textwrap
+
+ft = expanded_dict[42]
+to = expanded_dict[31]
+all = ft+to
+results = []
+print("------->", len(cases.split("\n")))
+for i in cases.split("\n"):
+    if len(i) % len(ft[0]) != 0:
+        continue
+
+    in_ft = True
+    found = True
+    to_count = 0
+    ft_count = 0
+
+    if i == "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa":
+        import ipdb;ipdb.set_trace()
+
+    for part in textwrap.wrap(i, len(ft[0])):
+        if part in to and in_ft == True:
+            to_count += 1
+            in_ft = False
+            continue
+
+        if part in to and in_ft == False:
+            to_count += 1
+            continue
+
+        if part in ft and in_ft == True:
+            ft_count += 1
+            continue
+
+        if part in ft and in_ft == False:
+            found = False
+            break
+
+        if part not in all:
+            found = False
+            break
+
+        import ipdb;ipdb.set_trace()
+        raise Exception("WTF?")
+
+    if found == False:
+        continue
+
+    if in_ft == False and ft_count > to_count:
+        results.append(i)
+
+
+results = set(results)
+pprint.pprint(results)
+
+print("Matches:", len(results))
